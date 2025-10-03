de38464b09fa11d11bcb6dd9f863f3fac3beee9e
itganeshtech <it.ganesh.tech@gmail.com>
2025-10-04 03:38:29 +0530

    Seeding Book Table

--- WikiDataAccess/Migrations/20251003220120_SeedBookTable.Designer.cs
+++ WikiDataAccess/Migrations/20251003220120_SeedBookTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wiki_DataAccess.Data;
 
@@ -10,9 +11,11 @@ using Wiki_DataAccess.Data;
 namespace Wiki_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251003220120_SeedBookTable")]
+    partial class SeedBookTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
