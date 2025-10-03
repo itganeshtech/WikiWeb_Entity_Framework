e7939bfeea57479f0da079429ff21a595a58d4a8
itganeshtech <it.ganesh.tech@gmail.com>
2025-10-04 01:43:19 +0530

    Book and Genre table created

--- WikiDataAccess/Data/ApplicationDbContext.cs
+++ WikiDataAccess/Data/ApplicationDbContext.cs
@@ -4,10 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wiki_Model.Models;
 
 namespace Wiki_DataAccess.Data
 {
     public class ApplicationDbContext:DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost;Database=WikiData;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+        }
     }
 }
